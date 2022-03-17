ee39086162357ded2136f4ce56c360a839ab0752
esau-morais <esaumorais7@gmail.com>
2022-03-17 18:54:40 -0300

    chore: add daily weather report

---
+++ src/main.js
@@ -0,0 +1,28 @@
+require("dotenv").config()
+const nodemailer = require("nodemailer");
+
+(async function main() {
+    console.log("🚀 Running weather report");
+
+    const transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+          user: process.env.MAIL_USER_EMAIL, 
+          pass: process.env.MAIL_USER_PASS, 
+        },
+      });
+
+      await transporter.sendMail({
+        from: `"Esaú Morais 🧑‍💻" <${process.env.MAIL_FROM}>`, // sender address
+        to: "esaumorais7@gmail.com", // list of receivers
+        subject: "✔ Daily weather report", // Subject line
+        text: `
+            Daily weather report
+        `, // plain text body
+        html: `
+            <h1>Daily weather report</h1>
+        `, // html body
+      });
+})()
